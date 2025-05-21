0938fc7d69a8ce627b45bbd57ef2022ba210e82d
Mmelian2 <micaela.melian02@gmail.com>
2025-05-21 18:34:53 -0300

    settings

--- proyecto_descuentos2/settings.py
+++ proyecto_descuentos2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','proyecto-des.onrender.com']
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
